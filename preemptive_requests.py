b560f70eb8f7148b0f41f64851b0c51d14aa7a2e
cristinasewell <csewell@slac.stanford.edu>
2021-03-10 00:11:29 -0800

    MNT: make only one custom QTableWidgetItem

--- preemptive_requests.py
+++ preemptive_requests.py
@@ -5,13 +5,23 @@ from pydm.widgets import PyDMEmbeddedDisplay
 from PyQt5.QtGui import QTableWidgetItem
 
 
-# TODO: maybe just have one class with an additional argument that will take the label
-# and the column?
-class RateTableWidgetItem(QTableWidgetItem):
+class CustomTableWidgetItem(QTableWidgetItem):
     """
     Custom QTableWidgetItem to allow sorting items in a QTableWidget
-    based on the Rate values from a PyDMEmbeddedDisplay widget.
+    based on the values from a PyDMEmbeddedDisplay widget.
+
+    Parameters
+    ----------
+    widget_type : QtWidgets
+        Type of widget, eg. `QtWidgets.QLabel`
+    widget_name : str
+        The name of the widget (object name).
     """
+    def __init__(self, widget_type, widget_name, parent=None):
+        QTableWidgetItem.__init__(self, parent)
+        self._obj_name = widget_name
+        self._obj_type = widget_type
+
     def __lt__(self, other):
         """
         Override the __lt__ to handle data sorting for rate.
@@ -20,32 +30,12 @@ class RateTableWidgetItem(QTableWidgetItem):
         column = 0
         try:
             other_widget = other.tableWidget().cellWidget(other.row(), column)
-            other_label = other_widget.embedded_widget.ui.findChild(QtWidgets.QLabel, f'rate_label')
+            other_label = other_widget.embedded_widget.ui.findChild(
+                self._obj_type, str(self._obj_name))
 
             widget = self.tableWidget().cellWidget(self.row(), column)
-            label = widget.embedded_widget.ui.findChild(QtWidgets.QLabel, f'rate_label')
-            return float(other_label.text()) < float(label.text())
-        except Exception:
-            return QTableWidgetItem.__lt__(self, other)
-
-
-class TransmissionRateTableWidgetItem(QTableWidgetItem):
-    """
-    Custom QTableWidgetItem to allow sorting items in a QTableWidget
-    based on the Transmission values from a PyDMEmbeddedDisplay widget.
-    """
-    def __lt__(self, other):
-        """
-        Override the __lt__ to handle data sorting for transmission.
-        """
-        # column 0 is where the embedded display widget data is at
-        column = 0
-        try:
-            other_widget = other.tableWidget().cellWidget(other.row(), column)
-            other_label = other_widget.embedded_widget.ui.findChild(QtWidgets.QLabel, f'transmission_label')
-
-            widget = self.tableWidget().cellWidget(self.row(), column)
-            label = widget.embedded_widget.ui.findChild(QtWidgets.QLabel, f'transmission_label')
+            label = widget.embedded_widget.ui.findChild(
+                self._obj_type, str(self._obj_name))
             return float(other_label.text()) < float(label.text())
         except Exception:
             return QTableWidgetItem.__lt__(self, other)
@@ -53,14 +43,16 @@ class TransmissionRateTableWidgetItem(QTableWidgetItem):
 
 class PreemptiveRequests(Display):
     def __init__(self, parent=None, args=None, macros=None):
-        super(PreemptiveRequests, self).__init__(parent=parent, args=args, macros=macros)
+        super(PreemptiveRequests, self).__init__(parent=parent,
+                                                 args=args, macros=macros)
         self.config = macros
         self.setup_ui()
 
     def setup_ui(self):
         self.setup_requests()
         self.ui.sort_rate_button.clicked.connect(self.sort_rate_items)
-        self.ui.sort_transm_button.clicked.connect(self.sort_transmission_items)
+        self.ui.sort_transm_button.clicked.connect(
+            self.sort_transmission_items)
 
     def setup_requests(self):
         if not self.config:
@@ -87,7 +79,8 @@ class PreemptiveRequests(Display):
             template = 'templates/preemptive_requests_entry.ui'
             for pool_id in range(pool_start, pool_end+1):
                 pool = str(pool_id).zfill(pool_zfill)
-                macros = dict(index=count, P=prefix, ARBITER=arbiter, POOL=pool)
+                macros = dict(index=count, P=prefix, ARBITER=arbiter,
+                              POOL=pool)
                 widget = PyDMEmbeddedDisplay(parent=reqs_table)
                 widget.macros = json.dumps(macros)
                 channel = f'ca://{prefix}{arbiter}:AP:Entry:{pool}:Live_RBV'
@@ -107,16 +100,19 @@ class PreemptiveRequests(Display):
                 reqs_table.insertRow(row_position)
                 reqs_table.setCellWidget(row_position, 0, widget)
 
-                # insert a fake QTableWidgetItem to be able to customize sorting
+                # insert a fake QTableWidgetItem to customize sorting
                 # based on Rate - column position 0
-                item = RateTableWidgetItem()
-                item.setSizeHint(widget.size())
-                reqs_table.setItem(row_position, 0, item)
-                # insert a fake QTableWidgetItem to be able to customize sorting
-                # based on Transmission - column position 1
-                rate_item = TransmissionRateTableWidgetItem()
+                rate_item = CustomTableWidgetItem(widget_type=QtWidgets.QLabel,
+                                                  widget_name='rate_label')
                 rate_item.setSizeHint(widget.size())
-                reqs_table.setItem(row_position, 1, rate_item)
+                reqs_table.setItem(row_position, 0, rate_item)
+                # insert a fake QTableWidgetItem to customize sorting
+                # based on Transmission - column position 1
+                trans_item = CustomTableWidgetItem(
+                    widget_type=QtWidgets.QLabel,
+                    widget_name='transmission_label')
+                trans_item.setSizeHint(widget.size())
+                reqs_table.setItem(row_position, 1, trans_item)
 
                 count += 1
         reqs_table.resizeRowsToContents()
@@ -125,16 +121,20 @@ class PreemptiveRequests(Display):
     def sort_rate_items(self, value):
         column = 0
         if value is True:
-            self.ui.reqs_table_widget.sortItems(column, QtCore.Qt.DescendingOrder)
+            self.ui.reqs_table_widget.sortItems(column,
+                                                QtCore.Qt.DescendingOrder)
         else:
-            self.ui.reqs_table_widget.sortItems(column, QtCore.Qt.AscendingOrder)
+            self.ui.reqs_table_widget.sortItems(column,
+                                                QtCore.Qt.AscendingOrder)
 
     def sort_transmission_items(self, value):
         column = 1
         if value is True:
-            self.ui.reqs_table_widget.sortItems(column, QtCore.Qt.DescendingOrder)
+            self.ui.reqs_table_widget.sortItems(column,
+                                                QtCore.Qt.DescendingOrder)
         else:
-            self.ui.reqs_table_widget.sortItems(column, QtCore.Qt.AscendingOrder)
+            self.ui.reqs_table_widget.sortItems(column,
+                                                QtCore.Qt.AscendingOrder)
 
     def ui_filename(self):
         return 'preemptive_requests.ui'
