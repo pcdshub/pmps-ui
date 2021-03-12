143c19c15cb6c0c8ce3e76edf56b3e36af943557
cristinasewell <csewell@slac.stanford.edu>
2021-03-12 15:52:15 -0800

    MNT: add a check to differentiate when values is changed internally vs externally - I think this will work now

--- line_beam_parameters.py
+++ line_beam_parameters.py
@@ -14,7 +14,7 @@ class LineBeamParametersControl(Display):
     Class to handle display for the Line Beam Parameters Control tab.
     """
     # object names for all energy range bits checkboxes, set them all
-    # to checked to start with
+    # to unchecked to start with
     _bits = {'bit15': False, 'bit14': False, 'bit13': False, 'bit12': False,
              'bit11': False, 'bit10': False, 'bit9': False, 'bit8': False,
              'bit7': False, 'bit6': False, 'bit5': False, 'bit4': False,
@@ -23,6 +23,9 @@ class LineBeamParametersControl(Display):
     # signal to emit when energy range is changed
     energy_range_signal = QtCore.Signal(int)
 
+    # use this to help differentiate between changing
+    # the bits internally vs. externally
+    _bits_are_set = False
     energy_channel = None
 
     def __init__(self, parent=None, args=None, macros=None):
@@ -40,7 +43,6 @@ class LineBeamParametersControl(Display):
         self.setup_bits_connections()
         self.setup_bit_indicators()
         self.setup_energy_range_channel()
-        self.energy_range_changed(47103)
 
     def setup_bits_connections(self):
         """
@@ -49,9 +51,13 @@ class LineBeamParametersControl(Display):
         """
         for key, item in self._bits.items():
             cb = self.findChild(QtWidgets.QCheckBox, f"{key}")
-            cb.stateChanged.connect(functools.partial(self.calc_energy_range, key))
+            cb.stateChanged.connect(functools.partial(
+                self.calc_energy_range, key))
 
     def setup_bit_indicators(self):
+        """
+        Borrowed function from fast_faults, to help morph the labels vertically
+        """
         for key in self._bits.keys():
             label = self.findChild(PyDMLabel, f"label_{key}")
             if label is not None:
@@ -76,23 +82,19 @@ class LineBeamParametersControl(Display):
         The checkboxes can be tri-states - here we use the states 0 and 2
         for unchecked and checked respectively.
         """
-        if self.energy_range_groupBox.isChecked():
-            status = True if state == 2 else False
-            self._bits[key] = status
-
-            bit_map = list(map(int, [item for key, item in self._bits.items()]))
-            out = 0
-            for bit in bit_map:
-                # Shift left by pushing zeros in
-                # from the right and let the leftmost bits fall off
-                # Sets each bit to 1 if one of two bits is 1
-                out = (out << 1) | bit
-            # This would set the value of this PV:
-            # 'ca://${PREFIX}BeamParamCntl:ReqBP:PhotonEnergyRanges' to out
-            self.energy_range_signal.emit(out)
-        else:
-            # we don't want to update this if the combo box is not checked
-            return
+        status = True if state == 2 else False
+        self._bits[key] = status
+        bit_map = list(map(int, [item for key, item in self._bits.items()]))
+        decimal_value = 0
+        for bit in bit_map:
+            # Shift left by pushing zeros in
+            # from the right and let the leftmost bits fall off
+            # Sets each bit to 1 if one of two bits is 1
+            decimal_value = (decimal_value << 1) | bit
+        # Emitting the signal below would set the value of this PV:
+        # 'ca://${PREFIX}BeamParamCntl:ReqBP:PhotonEnergyRanges' to out
+        if not self._bits_are_set:
+            self.energy_range_signal.emit(decimal_value)
 
     def setup_energy_range_channel(self):
         prefix = self.config.get('line_arbiter_prefix')
@@ -102,49 +104,35 @@ class LineBeamParametersControl(Display):
             'ca://${PREFIX}BeamParamCntl:ReqBP:PhotonEnergyRanges').safe_substitute(**ch_macros)
         self.energy_channel = PyDMChannel(
             ch,
-            connection_slot=self.connected_energy_range,
             value_slot=self.energy_range_changed,
             value_signal=self.energy_range_signal
         )
         self.energy_channel.connect()
-        # TODO: remove this - only for testing
-        self.energy_range_signal.connect(self.catch_the_signal)
-
-    def connected_energy_range(self):
-        # getting in here when the connection of energy_range changes
-        # TODO: probably don't need this method at all
-        ...
-
-    # TODO: remove this - only for testing
-    @QtCore.Slot(int)
-    def catch_the_signal(self, energy_range):
-        print(energy_range)
 
     def energy_range_changed(self, energy_range):
         """
-        Connect to this slot only ones - at first, then disconnect.
-        """
-        # TODO i might need this at first to set the bits initially - not sure yet.
-        # can i connect this ones and then disconnect it right away?
-        print(f'checked: {self.energy_range_groupBox.isChecked()}')
-        if self.energy_range_groupBox.isChecked() is False:
-            print(f'The range changed {energy_range}')
+        This slot is supposed to handled the initial value of the
+        Photon Energy Range coming in as soon as we connect, as well
+        as whenever this value is changed outside this application.
 
-            binary_range = list(bin(energy_range).replace("0b", ""))
-            binary_list = list(map(int, binary_range))
-            print(binary_list)
-            # status = True if state == 2 else False
-            for key, status in zip(self._bits.keys(), binary_list):
-                self._bits[key] = bool(status)
-                cb = self.findChild(QtWidgets.QCheckBox, f"{key}")
-                state = 2 if status == 1 else 0
-                print(state)
-                cb.setCheckState(state)
-            print(self._bits)
-        else:
-            # do nothing here if it is checked...
-            # TODO: it would be nice to disconnect this guy here after it got the first value
+        Parameters
+        ----------
+        energy_range : int
+            The decimal value of the photon energy range.
+        """
+        if energy_range is None:
             return
+        binary_range = list(bin(energy_range).replace("0b", ""))
+        binary_list = list(map(int, binary_range))
+        self._bits_are_set = True
+        for key, status in zip(self._bits.keys(), binary_list):
+            self._bits[key] = bool(status)
+            cb = self.findChild(QtWidgets.QCheckBox, f"{key}")
+            state = 2 if status == 1 else 0
+            cb.setCheckState(state)
+        # set this value back to false so we don't create a continuous
+        # loop between this slot and the energy_range_signal signal.
+        self._bits_are_set = False
 
     def ui_filename(self):
         return 'line_beam_parameters.ui'
