d51ca2be4ed51545d173032913e2923eb983aa83
Zachary Lentz <zlentz@slac.stanford.edu>
2022-10-18 14:08:55 -0700

    ENH: tee so I see the output interactively, redirect stderr to stdout into the logfile

--- nfs_afs_sync.sh
+++ nfs_afs_sync.sh
@@ -5,11 +5,11 @@ LOG="${HERE}/sync_log.txt"
 SOURCE="${HERE}"
 DEST="/afs/slac/g/lcls/tools/pydm/display/xray/pmps-ui"
 
-echo "Sync at $(date)" >> "${LOG}"
-echo "from ${SOURCE}" >> "${LOG}"
-echo "to ${DEST}" >> "${LOG}"
+echo "Sync at $(date)" | tee -a "${LOG}"
+echo "from ${SOURCE}" | tee -a "${LOG}"
+echo "to ${DEST}" | tee -a "${LOG}"
 # Pick up permissions to write to afs
 aklog
 # Avoid git repo + random junk like screenshots
-rsync -rv ${SOURCE}/*.py ${SOURCE}/*.ui ${SOURCE}/*.yml ${SOURCE}/*.sh ${SOURCE}/templates "${DEST}" >> "${LOG}"
+rsync -rv ${SOURCE}/*.py ${SOURCE}/*.ui ${SOURCE}/*.yml ${SOURCE}/*.sh ${SOURCE}/templates "${DEST}" 2>&1 | tee -a "${LOG}"
 echo "---------------------------" >> "${LOG}"
