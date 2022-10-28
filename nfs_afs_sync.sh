4355b26c1c114a895ae65cfcd236dd48baf1ed06
Zachary Lentz <zlentz@slac.stanford.edu>
2022-10-28 16:11:39 -0700

    ENH: split script for better behavior with respect to cron email

--- nfs_afs_sync.sh
+++ nfs_afs_sync.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 # rsync the files in the deployed directory to afs to be picked up by acr
 HERE="$(readlink -f "$(dirname -- "${BASH_SOURCE[0]}")")"
-LOG="${HERE}/sync_log.txt"
 SOURCE="${HERE}"
 DEST="/afs/slac/g/lcls/tools/pydm/display/xray/pmps-ui"
 
-echo "Sync at $(date)" | tee -a "${LOG}"
-echo "from ${SOURCE}" | tee -a "${LOG}"
-echo "to ${DEST}" | tee -a "${LOG}"
-# Pick up permissions to write to afs (in case user hasn't done aklog yet)
-aklog -d 2>&1 | tee -a "${LOG}"
+echo "Sync at $(date)"
+echo "from ${SOURCE}"
+echo "to ${DEST}"
 # Avoid git repo + random junk like screenshots
-rsync -rv ${SOURCE}/*.py ${SOURCE}/*.ui ${SOURCE}/*.yml ${SOURCE}/*.sh ${SOURCE}/templates "${DEST}" 2>&1 | tee -a "${LOG}"
+rsync -rv ${SOURCE}/*.py ${SOURCE}/*.ui ${SOURCE}/*.yml ${SOURCE}/*.sh ${SOURCE}/templates "${DEST}"
 echo "---------------------------" >> "${LOG}"
