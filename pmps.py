cc4849ee1e122778ce9f9a31745c82ea6d55123e
Zachary Lentz <zlentz@slac.stanford.edu>
2023-09-22 15:16:03 -0700

    ENH: bring transmission override into solo tab, add context, minor cleanup

--- pmps.py
+++ pmps.py
@@ -177,12 +177,13 @@ class PMPS(Display):
         self.setup_arbiter_outputs()
         self.setup_ev_calculation()
         self.setup_line_parameters_control()
+        self.setup_trans_override()
         self.setup_plc_ioc_status()
         self.setup_beam_class_table()
 
         dash_url = self.config.get('dashboard_url')
         if self.user_args.no_web or dash_url is None:
-            self.ui.tab_arbiter_outputs.removeTab(7)
+            self.ui.tab_arbiter_outputs.removeTab(8)
         else:
             self.setup_grafana_log_display()
 
@@ -220,6 +221,12 @@ class PMPS(Display):
         tab = self.ui.tb_line_beam_param_ctrl
         beam_widget = LineBeamParametersControl(macros=self.config)
         tab.layout().addWidget(beam_widget)
+
+    def setup_trans_override(self):
+        from trans_override import TransOverride
+        tab = self.ui.tb_trans_override
+        jf_widget = TransOverride(macros=self.config)
+        tab.layout().addWidget(jf_widget)
 
     def setup_plc_ioc_status(self):
         from plc_ioc_status import PLCIOCStatus
