155ae90e2326bebf59bbc412d475a337d39d90cf
Zachary Lentz <zlentz@slac.stanford.edu>
2023-11-02 14:17:45 -0700

    DOC: detailed docstrings

--- templates/arbiter_outputs_entry.py
+++ templates/arbiter_outputs_entry.py
@@ -1,3 +1,10 @@
+"""
+Embedded pydm screen for a single row in the Arbiter Outputs table.
+
+Rather than putting this functionality into arbiter_outputs.py and reaching
+into the embedded widgets, it is cleaner to include it here and apply the
+behavior to the ui file in a more natural way.
+"""
 from __future__ import annotations
 
 from pydm import Display
@@ -6,6 +13,16 @@ from qtpy.QtCore import QObject, QTimer, Signal
 
 
 class ArbiterRow(Display):
+    """
+    PyDM display that represents one row in the Arbiter Outputs table.
+
+    This class is responsible for adding PyDM connections to all of the
+    fast fault PVs and keeping track of their status via counting.
+
+    The ui file this class uses has additional widgets for indicator
+    lights and text display that are paramterized via macros like normal
+    code-free pydm screens.
+    """
     fault_summaries: list[FaultSummary]
     fault_counters: list[CounterElement]
     faults: list[bool]
@@ -29,10 +46,22 @@ class ArbiterRow(Display):
         self.is_connected = []
         self.setup_ui()
 
-    def setup_ui(self):
+    def setup_ui(self) -> None:
+        """Standard-use catch-all method name for qt startup actions."""
         self.setup_counters()
 
-    def setup_counters(self):
+    def setup_counters(self) -> None:
+        """
+        Connect to the fast fault PVs and initialize the counters.
+
+        This sets up the initial conditions for the async chain of
+        next_counter_soon calls that split up the widget setup into
+        multiple qt events. It is implemented in this way so that the
+        render events and the user's input events can happen during
+        setup while this tab loads in the background.
+
+        You can think of this like a fancy for loop.
+        """
         self.prefix = self.config["P"]
         self.ffo = self.config["FFO"]
         self.ff_start = self.config["ff_start"]
@@ -43,11 +72,37 @@ class ArbiterRow(Display):
 
         self.next_counter_soon()
 
-    def next_counter_soon(self):
+    def next_counter_soon(self) -> None:
+        """
+        Creates a counter by calling setup_next_counter on the qt event queue.
+
+        Combined with setup_next_counter, this starts or continues a chain of
+        counter instantiations that ends when we exhaust our list of
+        configured PVs.
+
+        The event will happen 1ms after this method call to give plenty
+        of time for user input and rendering events, while not being so long
+        as to drag out the chain of initialization.
+
+        A reference is held to the QTimer to prevent garbage collection,
+        but I'm not entirely sure this is needed.
+        """
         self.setup_counter_timer = QTimer()
         self.setup_counter_timer.singleShot(1, self.setup_next_counter)
 
-    def setup_next_counter(self):
+    def setup_next_counter(self) -> None:
+        """
+        Adds one additional counting source, then calls next_counter_soon.
+
+        Combined with next_counter_soon, this starts or continues a chain of
+        counter instantiations that ends when we exhaust our list of
+        configured PVs.
+
+        Each counting source is a group of PyDM channels that consume
+        EPICS PVs with information pertaining to fast faults.
+        These are then aggregated with all the other counting sources to get
+        a total count of e.g. the number of faulting channels, etc.
+        """
         fault_num_str = str(self.fault_num).zfill(self.zfill)
         # Use the bypass channel to get the bypass counts
         bypass_counter = CounterElement(
@@ -99,7 +154,7 @@ class ArbiterRow(Display):
         self._channels.append(ic_ch)
 
         # Combine the ok and in_use channels to get the fault counts
-        # We are fauling if ok=False and in_use=True
+        # We are faulting if ok=False and in_use=True
         # Summarize this as a combined local channel
         fault_summary = FaultSummary(
             ok_pvname=f"ca://{self.prefix}FFO:{self.ffo}:FF:{fault_num_str}:OK_RBV",
@@ -145,28 +200,82 @@ class ArbiterRow(Display):
             self.next_counter_soon()
 
     def show_loc_connected(self, *args, **kwargs):
+        """
+        No-op slot to put on loc:// channels so that they show as connected.
+
+        Otherwise, they can show as disconnected even if the value updates
+        properly.
+        """
         ...
 
     def new_fault_count(self, count: int):
+        """
+        Slot to show a "major" alarm when the total fault count is nonzero.
+        """
         if count:
             self.ui.fault_label.alarm_severity_changed(2)
         else:
             self.ui.fault_label.alarm_severity_changed(0)
 
     def new_bypass_count(self, count: int):
+        """
+        Slot to show a "minor" alarm when the total bypass count is nonzero.
+        """
         if count:
             self.ui.bypass_label.alarm_severity_changed(1)
         else:
             self.ui.bypass_label.alarm_severity_changed(0)
 
     def channels(self):
+        """
+        Callable method to return a list of open PyDMChannel instances.
+
+        This is sometimes used by PyDM to find channels to clean up.
+        """
         return self._channels
 
     def ui_filename(self):
+        """
+        Return the name of the ui file to load for PyDM.
+        """
         return 'arbiter_outputs_entry.ui'
 
 
 class CounterElement(QObject):
+    """
+    A helper object for including one channel's value in the shared counts.
+
+    Each data source that contributes to a count should have one
+    CounterElement that points to the unique channel address and to a shared
+    value list and optionally a shared connection list.
+
+    Then, whenever that value source updates (and optionally when the
+    connection state changes), this class will update the shared list
+    and emit the total count from its value_sig (and optionally the
+    conn_sig too.) This design was chosen to avoid race conditions with
+    counters and counter statuses. For example, if two counters increment at
+    the same time, they will both emit the correct sum regardless of the
+    order they reach the emit method call.
+
+    The data source is expected to supply integers that are either 1 or 0.
+
+    Parameters
+    ----------
+    pvname : str
+        The PyDM channel address to use as the source of data.
+    index : int
+        The index in the various caches that this counter should write
+        its state to.
+    value_cache : list of int
+        A shared list of all the values from all of the different counter
+        elements that contribute to the shared count.
+    conn_cache : list of int, optional
+        If provided, this is the same as the value_cache except it is used
+        to track connection status.
+    parent : QObject, optional
+        Standard qt parent argument. If provided, it makes this object
+        a child object of the parent.
+    """
     value_sig = Signal(int)
     conn_sig = Signal(int)
 
@@ -185,6 +294,17 @@ class CounterElement(QObject):
         self.conn_cache = conn_cache
 
     def create_channel(self) -> PyDMChannel:
+        """
+        Create and return the PyDMChannel object.
+
+        Once connected, this channel will start collecting values
+        for the shared lists and this object's signals will be
+        active.
+
+        It is best to create and connect the channels only after
+        downstream consumers are ready to use the values from value_sig
+        and conn_sig.
+        """
         if self.conn_cache is None:
             return PyDMChannel(
                 address=self.pvname,
@@ -198,15 +318,44 @@ class CounterElement(QObject):
             )
 
     def new_value(self, value: int) -> None:
+        """
+        When a new value is recieved, stash it and emit the total count.
+        """
         self.value_cache[self.index] = value
         self.value_sig.emit(sum(self.value_cache))
 
     def new_conn(self, conn: int) -> None:
+        """
+        When a new connection state is recieved, stash it and emit the total count.
+        """
         self.conn_cache[self.index] = conn
         self.conn_sig.emit(sum(self.conn_cache))
 
 
 class FaultSummary(QObject):
+    """
+    Summarize the fault state of a single fast fault.
+
+    This is a combination of the OK signal and the IN_USE signal.
+    A signal is faulting if it is "IN_USE" but "NOT OK".
+
+    This is important because all faults that are "NOT IN_USE" are also
+    "NOT OK" by default, creating a lot of false positives for the fault
+    counter if we neglect to consider the IN_USE signal.
+
+    Parameters
+    ----------
+    ok_pvname : str
+        The PyDM channel associated with the "OK" fault signal, which is
+        1 when the condition is OK and 0 when we are faulting.
+    in_use_pvname : str
+        The PyDM channel associated with the "IN_USE" fault signal,
+        which is 1 when the fault's OK state is valid and is 0 when the
+        fault's OK state is invalid.
+    parent : QObject, optional
+        Standard qt parent argument. If provided, it makes this object
+        a child object of the parent.
+    """
     value_sig = Signal(int)
     is_ok: int
     is_in_use: int
@@ -224,6 +373,12 @@ class FaultSummary(QObject):
         self.is_in_use = 0
 
     def create_channels(self) -> tuple[PyDMChannel, PyDMChannel]:
+        """
+        Return two channels for the OK and IN_USE signals respectively.
+
+        Once connected, these channels will start collecting values
+        to combine for the aggregate value_sig output.
+        """
         return PyDMChannel(
             address=self.ok_pvname,
             value_slot=self.new_ok,
@@ -233,14 +388,23 @@ class FaultSummary(QObject):
         )
 
     def new_ok(self, value: int):
+        """
+        When we recieve a new value from the OK signal, stash and update.
+        """
         self.is_ok = value
         self.new_fault()
 
     def new_in_use(self, value: int):
+        """
+        When we recieve a new value from the IN_USE signal, stash and update.
+        """
         self.is_in_use = value
         self.new_fault()
 
     def new_fault(self):
+        """
+        Update consumers of value_sig with the current fault state.
+        """
         if self.is_in_use:
             self.value_sig.emit(1 - self.is_ok)
         else:
